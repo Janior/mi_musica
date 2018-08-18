edc68c7e895ccd4f0452cbb2c335309f6f763a4e
Janior <janior_orlando@hotmail.com>
2018-08-17 20:44:43 -0700

    Notice y crear nuevo artista

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,12 +4,26 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artists = Artist.new
+    @artist = Artist.new
   end
 
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artists_path, notice: "El artista fue creado con EXITO!"
+    else
+      render :new
+    end
+  end
 
   def show
     @artist = Artist.find(params[:id])
     @albums = @artist.albums.ids
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :image_url)
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/artists', to: 'artists#index'
-  get '/artists/new', to: 'artists#new'
-
+  get '/artists/new', to: 'artists#new', as: 'new_artist'
+  post '/artists', to: 'artists#create'
 
 end
